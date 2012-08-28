30d571776c0684f242b1781415929fa3a609979c
TxusMaista <TxusMaista@TxPro.local>
2012-08-28 13:08:25 +0200

    watermark

--- kinout-1.0.js
+++ kinout-1.0.js
@@ -8,7 +8,7 @@
         }}
 }).call(this);
 (function() {
-    KINOUT.Constants = {MARKUP: {GLOW: '<div class="glow"></div>',COPYRIGHT: '<div class="copyright"><a href="http://kinout.tapquo.com/">kinout</a>by tapquo</div>',PROGRESS: {horizontal: "<div class='progress horizontal' value='0' max='100'><span></span></div>",vertical: "<div class='progress vertical' value='0' max='100'><span></span></div>"}},SELECTOR: {KINOUT: ".kinout",SLIDE: ".kinout>section",SUBSLIDE: ".kinout>section.present>article",STEP: "section.present > article.present [data-step]",STEP_TO_SHOW: ":not([data-run='success'])",
+    KINOUT.Constants = {MARKUP: {GLOW: '<div class="glow"></div>',COPYRIGHT: '<div class="copyright"><img style="border-radius:5px;" width="100px" src="assets/image/mu.jpg" /></div>',PROGRESS: {horizontal: "<div class='progress horizontal' value='0' max='100'><span></span></div>",vertical: "<div class='progress vertical' value='0' max='100'><span></span></div>"}},SELECTOR: {KINOUT: ".kinout",SLIDE: ".kinout>section",SUBSLIDE: ".kinout>section.present>article",STEP: "section.present > article.present [data-step]",STEP_TO_SHOW: ":not([data-run='success'])",
             STEP_TO_HIDE: "[data-run='success']",PROGRESS: {horizontal: ".progress.horizontal",vertical: ".progress.vertical"}},STYLE: {PAST: "past",PRESENT: "present",FUTURE: "future"}}
 }).call(this);
 (function() {
